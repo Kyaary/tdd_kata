e060929735ca1df6a690f02c9cb7107a4afab9ed
Kyary <kyary@kyary.com>
2020-03-06 14:30:01 +0100

    Green phase 3

--- Leap Years/test_LeapYears.py
+++ Leap Years/test_LeapYears.py
@@ -3,6 +3,8 @@ def isBissextile(year):
         return True
     elif year%4==0 and year%100!=0:
         return True
+    elif year%100==0 and year%400!=0:
+        return False
 
 def test_should_year_is_multiple_of_400():
     assert isBissextile(400) == True
@@ -11,4 +13,4 @@ def test_should_year_is_divisible_by_4_but_not_by_100():
     assert isBissextile(2008) == True
 
 def test_should_year_be_divisible_by_100_but_not_by_400():
-    assert isBissextile(1900) == False
+    assert isBissextile(1800) == False
