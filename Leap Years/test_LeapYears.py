b932c0d36f8a4d1e546ade94025f3eaf08f7a23b
Kyary <kyary@kyary.com>
2020-03-06 12:55:57 +0100

    Red phase 2

--- Leap Years/test_LeapYears.py
+++ Leap Years/test_LeapYears.py
@@ -1,6 +1,9 @@
 def isBissextile(year):
-    if x%400 == 0:
+    if year%400 == 0:
         return True
 
 def test_year_is_multiple_of_400():
     assert isBissextile(400) == True
+
+def test_year_is_divisible_by_4_but_not_by_100():
+    assert isBissextile(2008) == True
