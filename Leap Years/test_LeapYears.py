c60560c92e0a26d163121eb017a931c927a11c58
Kyary <kyary@kyary.com>
2020-03-06 14:13:23 +0100

    Blue phase 2 : nom fonction de test

--- Leap Years/test_LeapYears.py
+++ Leap Years/test_LeapYears.py
@@ -4,8 +4,8 @@ def isBissextile(year):
     elif year%4==0 and year%100!=0:
         return True
 
-def test_year_is_multiple_of_400():
+def test_should_year_is_multiple_of_400():
     assert isBissextile(400) == True
 
-def test_year_is_divisible_by_4_but_not_by_100():
+def test_should_year_is_divisible_by_4_but_not_by_100():
     assert isBissextile(2008) == True
