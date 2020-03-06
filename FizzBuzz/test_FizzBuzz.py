07c54d341d06f2b2fab5d06c8a74dde105d24b74
Kyary <kyary@kyary.com>
2020-03-06 12:29:56 +0100

    Green phase 4

--- FizzBuzz/test_FizzBuzz.py
+++ FizzBuzz/test_FizzBuzz.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 
 def fizzBuzz(input_to_test):
+    if input_to_test%3==0:
+        if input_to_test%5==0:
+            return "FizzBuzz"
     if input_to_test%3 == 0:
         return "Fizz"
-    elif input_to_test%5 == 0:
-        return "Buzz"    
+    if input_to_test%5 == 0:
+        return "Buzz"
     return ""
 
 def test_return_empty_string_when_is_not_divisible():
     assert fizzBuzz(1) == ""
 
 def test_should_return_fizz_if_is_divisible_by_3():
-    assert fizzBuzz(3) == "Fizz"
+    assert fizzBuzz(18) == "Fizz"
 
 def test_should_return_buzz_if_is_divisible_by_5():
-    assert fizzBuzz(5) == "Buzz"
+    assert fizzBuzz(35) == "Buzz"
 
 def test_should_return_fizzBuzz_if_is_divisible_by_15():
-    assert fizzBuzz(15) == "fizzBuzz"
+    assert fizzBuzz(30) == "FizzBuzz"
